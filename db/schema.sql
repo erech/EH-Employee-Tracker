48b2eab434395707bc664664e3fcad5b6369dda0
erech <sofiahanapin@gmail.com>
2023-02-26 21:58:39 -0800

    add init code +files

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS corporate_db
+CREATE DATABASE corporate_db
+USE corporate_db
+
+DROP TABLE IF EXISTS employee
+DROP TABLE IF EXISTS role
+DROP TABLE IF EXISTS department
+
+
+
+CREATE TABLE department (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  department_name VARCHAR(100) UNIQUE NOT NULL
+)
+
+CREATE TABLE employee {
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  role_id INT,
+  manager_id INT
+}
+
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_in (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) 
+  REFERENCES department(id) ON DELETE CASCADE
+)
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (dept_name)
+VALUES 
+('Formulations'),
+('QC/QA'),
+('Product Support'),
+('R&D');
+
+
+INSERT INTO job (title, salary, department_id)
+VALUES 
+('Engineer Scientist', 70000, 1),
+('Chemist', 70000, 2),
+('Researcher', 85000, 3),
+('Senior Engineer', 175000, 1),
+('Senior Chemist', 175000, 2),
+('Staff Scientist', 200000, 3);
+
+INSERT INTO employee (first_name, last_name, job_id, manager_id)
+VALUES 
+('Rick', 'Chen', 1, NULL),
+('Johnny', 'Suh', 2, 1),
+('Elon', 'Musk', 3, 2),
+('Jelly', 'Fish', 4, 3),
+('Aaron', 'One', 5, 4),
+('Aaron', 'Two', 6, NULL),
+('Jennifer', 'Give', 7, NULL);
